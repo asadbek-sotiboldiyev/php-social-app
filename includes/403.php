d799031bc99b4af4b2dc60f1cbc39c84d824d430
AsAdbEk Sotiboldiyev <aybekovichas0209@gmail.com>
2024-03-05 00:44:34 +0500

    New commit

--- includes/403.php
+++ includes/403.php
@@ -1,3 +1,6 @@
-<h1>403 | THIS ACTION IS UNAUTHORIZED.</h1>
-<br>
-<a href="/">BACK</a>
+<link rel="stylesheet" href="../static/style.css">
+<div class="container">
+    <h1>403 | THIS ACTION IS UNAUTHORIZED.</h1>
+    <br>
+    <a href="/">BACK</a>
+</div>
--- includes/404.php
+++ includes/404.php
@@ -1,3 +1,6 @@
-<h1>404 page not found</h1>
-<br>
-<a href="/">BACK</a>
+<link rel="stylesheet" href="../static/style.css">
+<div class="container">
+    <h1>404 page not found</h1>
+    <br>
+    <a href="/">BACK</a>
+</div>
